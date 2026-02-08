e4530ed815f3b966a1f820710619cac6c720cf40
Tamanna Parashar <thisistamanna.03@gmail.com>
2026-02-08 20:25:25 +0530

    updated environment variables

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose"
-const url = "mongodb://localhost:27017/moodboard"
-mongoose.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+const url = process.env.MONGO_URL
+mongoose.connect(url);
 const db = mongoose.connection
 db.on("connected",()=>{
     console.log("Connected to MongoDB");
