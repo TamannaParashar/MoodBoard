1748269323a776cd0c6d99991ba0d16a58362200
Tamanna Parashar <thisistamanna.03@gmail.com>
2026-04-04 00:13:57 +0530

    updation

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false, crypto: false };
+    config.resolve.fallback = { fs: false, path: false, crypto: false, encoding: false };
     return config;
   },
 };
